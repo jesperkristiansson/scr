a7402ae878124b16ae5e559307bcab3a859eb642
Jesper Kristiansson <jeppe.00@live.se>
2024-05-09 14:21:05 +0200

    enable server and client to connect

--- src/client.cc
+++ src/client.cc
@@ -3,24 +3,61 @@
 #include <iostream>
 #include <string>
 
+#include <cstdint>
+#include <cstring>
 #include <cassert>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
 int main(int argc, char **argv){
     assert(argc == 3);
     //add error checking for IP address
-    std::string ip = argv[1];
-    unsigned int port;
+    char *ip = argv[1];
+    int port_int;
     try
     {
-        port = std::stoi(argv[2]);
+        port_int = std::stoi(argv[2]);
     }
     catch(const std::exception& e)
     {
         FAIL(e.what());
     }
 
+    uint16_t port;
+    if(port_int < 0 || port_int > UINT16_MAX){
+        FAIL("Port not in range (0-65535)");
+    }
+    port = (uint16_t)port_int;
+
     std::cout << "Client connecting to " << ip << ":" << port << std::endl;
+
+    int client_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if(client_fd == -1){
+        perror("socket()");
+        exit(EXIT_FAILURE);
+    }
+
+    struct sockaddr_in server_sa;
+    memset(&server_sa, 0, sizeof(server_sa));
+    server_sa.sin_family = AF_INET;
+    server_sa.sin_port = htons(port);
+    if(inet_aton(ip, &server_sa.sin_addr) == 0){  //right?
+        FAIL("inet_aton()");
+    }
+
+    int server_fd = connect(client_fd, (struct sockaddr *)&server_sa, sizeof(server_sa));
+    if(server_fd == -1){
+        perror("connect()");
+        exit(EXIT_FAILURE);
+    }
+
+    std::cout << "Connected to: " << inet_ntoa(server_sa.sin_addr) << ":" << ntohs(server_sa.sin_port) << std::endl;
+
+    close(server_fd);
+    close(client_fd);
 
     return EXIT_SUCCESS;
 }
--- src/server.cc
+++ src/server.cc
@@ -3,22 +3,77 @@
 #include <iostream>
 #include <string>
 
+#include <cstdint>
+#include <cstring>
 #include <cassert>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
 int main(int argc, char **argv){
     assert(argc == 2);
-    unsigned int port;
+    int port_int;
     try
     {
-        port = std::stoi(argv[1]);
+        port_int = std::stoi(argv[1]);
     }
     catch(const std::exception& e)
     {
         FAIL(e.what());
     }
 
+    uint16_t port;
+    if(port_int < 0 || port_int > UINT16_MAX){
+        FAIL("Port not in range (0-65535)");
+    }
+    port = (uint16_t)port_int;
+
     std::cout << "Server running at port " << port << std::endl;
+
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if(server_fd == -1){
+        perror("socket()");
+        exit(EXIT_FAILURE);
+    }
+
+    const int enable = 1;
+    if(setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(enable)) == -1){
+        perror("setsockopt(SO_REUSEADDR)");
+        exit(EXIT_FAILURE);
+    }
+
+    struct sockaddr_in server_sa;
+    memset(&server_sa, 0, sizeof(server_sa));
+    server_sa.sin_family = AF_INET;
+    server_sa.sin_port = htons(port);
+    server_sa.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    if(bind(server_fd, (struct sockaddr *)&server_sa, sizeof(server_sa)) == -1){
+        perror("bind()");
+        exit(EXIT_FAILURE);
+    }
+
+    if(listen(server_fd, 32) == -1){
+        perror("listen()");
+        exit(EXIT_FAILURE);
+    }
+
+    struct sockaddr_in client_sa;
+    socklen_t client_sa_len;
+    client_sa_len = sizeof(client_sa);
+    int client_fd = accept(server_fd, (struct sockaddr *)&client_sa, &client_sa_len);
+    if(client_fd == -1){
+        perror("accept()");
+        exit(EXIT_FAILURE);
+    }
+
+    std::cout << "Client connected: " << inet_ntoa(client_sa.sin_addr) << ":" << ntohs(client_sa.sin_port) << std::endl;
+
+
+    close(client_fd);
+    close(server_fd);
 
     return EXIT_SUCCESS;
 }
